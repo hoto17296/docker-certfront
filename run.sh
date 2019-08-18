1c593596cb64af0dd66c077164a96acf286dd613
hoto17296 <mail@hoto.me>
2019-08-18 22:53:05 +0900

    support static file serving

--- run.sh
+++ run.sh
@@ -12,5 +12,12 @@ sed -i -e "s/DOMAIN/${DOMAIN}/g" ${NGINX_CONF}
 sed -i -e "s/APP_HOST/${APP_HOST}/g" ${NGINX_CONF}
 sed -i -e "s/APP_PORT/${APP_PORT:-80}/g" ${NGINX_CONF}
 
+if [[ -n "${STATIC_PATH}" && -n "${STATIC_ROOT}" ]]
+then
+  sed -i -e "s|STATIC_SERVE|location ${STATIC_PATH} { root ${STATIC_ROOT}; }|g" ${NGINX_CONF}
+else
+  sed -i -e "s|STATIC_SERVE||g" ${NGINX_CONF}
+fi
+
 crond
 nginx -c ${NGINX_CONF}
